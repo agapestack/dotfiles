6db3c7d93e564d622bdffc6ce7463637b92a1c63
Agape <agapestack@gmail.com>
2023-08-21 17:12:34 +0200

    start to add vlc to waybar --> TODO: update mediaplayer.py to get vlc title

--- .config/waybar/mediaplayer.py
+++ .config/waybar/mediaplayer.py
@@ -103,7 +103,7 @@ class PlayerManager:
         current_player = self.get_first_playing_player()
         if current_player is not None:
             self.on_metadata_changed(current_player, current_player.props.metadata)
-        else:    
+        else:
             self.clear_output()
 
     def on_metadata_changed(self, player, metadata, _=None):
@@ -116,15 +116,15 @@ class PlayerManager:
         if player_name == "spotify" and "mpris:trackid" in metadata.keys() and ":ad:" in player.props.metadata["mpris:trackid"]:
             track_info = "Advertisement"
         elif artist is not None and title is not None:
-            track_info = f"{artist} - {title}"
+            track_info = f" {artist} - {title} "
         else:
             track_info = title
 
         if track_info:
             if player.props.status == "Playing":
-                track_info = " " + track_info
+                track_info = track_info + " "
             else:
-                track_info = " " + track_info
+                track_info = track_info + " "
         # only print output if no other player is playing
         current_playing = self.get_first_playing_player()
         if current_playing is None or current_playing.props.player_name == player.props.player_name:
